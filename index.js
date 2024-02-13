75d807fd60de9c1edbcc432b9377112464c378d4
Navaharish <navaharish2005@gmail.com>
2024-02-13 12:57:50 +0530

    dictionary app ready

---
+++ index.js
@@ -0,0 +1,59 @@
+var inp = document.getElementById("inp")
+let btn = document.getElementById("search_btn");
+const result = document.getElementById("result")
+const sound = document.getElementById("sound")
+const URL = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+
+
+dataFetch = () => {
+    const word = inp.value
+
+    fetch(`${URL}${word}`)
+        .then(data => data.json())
+        .then(item => {
+            const synonyms = item[0].meanings[0].synonyms.map(synonym => `${synonym}`).join(' , ');
+
+            result.innerHTML = `<div class="dict_container_word">
+                <h3>${word}</h3>
+
+                <button type="button" onClick="soundPlay()" ><i class="fa-solid fa-volume-high"></i></button>
+            </div>
+
+            <div class="dict_container_detail">
+           
+                <p>${item[0].meanings[0].partOfSpeech}</p>
+                <p>/${item[0].phonetic
+                }</p >
+            </div >
+
+            <div class="dict_container_meaning" id="meaning">
+                
+             ${item[0].meanings[0].definitions[0].definition}
+            </div >
+
+                  <h5>synonyms : </h5 >
+                  <p style="color:grey">${synonyms || 'Not found'} </p>
+
+    <div class="dict_container_example">
+        ${item[0].meanings[0].definitions[0].example || ''
+                }
+    </div > `
+            sound.setAttribute("src", `https:${item[0].phonetics[0].audio}`)
+        })
+        .catch(() => {
+            result.innerHTML = `< h3 class="error" > oops! Word is not found 😦</h3 > `
+        })
+
+}
+inp.addEventListener('keypress', (event) => {
+    if (event.key == "Enter") {
+        dataFetch()
+    }
+})
+btn.addEventListener("click", () => {
+    dataFetch()
+})
+
+function soundPlay() {
+    sound.play()
+}
